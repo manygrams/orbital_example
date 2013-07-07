aa6c888ee8f6d7e753eb1ff221fd6a9b7fd02d9d
Nick Evans <nicolas.evans@shopify.com>
2013-07-07 14:39:34 -0400

    Add progressbar and integrate R better (get rid of CSV output)

--- orbital_example.rb
+++ orbital_example.rb
@@ -25,29 +25,45 @@ SIM_NUM = ARGV[0] ? ARGV[0].to_i : 10000
 N = ARGV[1] ? ARGV[1].to_i : 15
 
 objects = {}
-
 N.times do |i|
   objects[i] = Astrodynamics::AstronomicalObject.new i
 end
 
 p = ProgressBar.new "Simulation", SIM_NUM
 
-CSV.open("planets.csv", "wb") do |csv|
-  SIM_NUM.times do | i |
-    p.set(i)
-    objects.each { | key, object | object.calc_tick(objects.except key) }
-    objects.each { | key, object | object.apply_tick }
-    objects.each { | key, object | csv << [ i, object.to_s, object.get_position[0], object.get_position[1], object.get_position[2] ] }
+t_vector = []
+name_vector = []
+x_vector = []
+y_vector = []
+z_vector = []
+
+SIM_NUM.times do | i |
+  p.set(i)
+  objects.each { | key, object | object.calc_tick(objects.except key) }
+  objects.each { | key, object | object.apply_tick }
+
+  objects.each do | key, object |
+    t_vector << i
+    name_vector << object.to_s
+    x_vector << object.get_position[0]
+    y_vector << object.get_position[1]
+    z_vector << object.get_position[2]
   end
 end
 
-R.eval <<EOF
-  rm(list=ls())
+R.echo false, false
+
+R.t = t_vector
+R.name = name_vector
+R.x = x_vector
+R.y = y_vector
+R.z = z_vector
+
+R.eval <<-EOF
   library(scatterplot3d)
   library(animation)
-  library(reshape2)
 
-  x <- read.csv("~/Code/orbital_example/planets.csv", header=FALSE)
+  x <- data.frame(t <- t, name <- name, x <- x, y <- x, z <- z)
   colnames(x) <- c("t","name","x","y","z")
 
   UNIQUE <- length(unique(x$name))
@@ -71,7 +87,7 @@ R.eval <<EOF
    scatterplot3d(this$x, this$y, this$z, xlab="x", ylab="y", zlab="z", xlim=xl, ylim=yl, zlim=zl)
   }
   ani.stop()
-  ani.options(interval = 0.1)
+  ani.options(interval = 0)
 EOF
 
 sleep(3)
