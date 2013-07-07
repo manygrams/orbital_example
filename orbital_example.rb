746221bc2104938e67390f16bb6fe5a25d9dc7b3
Nick Evans <nicolas.evans@shopify.com>
2013-07-06 21:25:35 -0400

    Added R code to ruby file

--- orbital_example.rb
+++ orbital_example.rb
@@ -12,16 +12,17 @@ end
 $: << 'lib'
 require 'astrodynamics/planet'
 require 'csv'
+require 'rinruby'
 
-N = 500000
+N = ARGV[0] ? ARGV[0].to_i : 10000
 
 objects =
 {
-  'sun'    => Astrodynamics::Planet.new('sun',     0,  0,    5, 1550, 0.5),
-  'moon1'  => Astrodynamics::Planet.new('moon1',  10, 10,  -20,  890, 0.25, -4e-7, 3e-05, -1e-05),
-  'moon2'  => Astrodynamics::Planet.new('moon2',   6, 18,    2,  500, 0.25, -1e-3, 2e-14,  2e-07),
-  'moon3'  => Astrodynamics::Planet.new('moon3', -10, 10,    8,   20, 0.05, -1e-3, 2e-14,  2e-07),
-  'moon4'  => Astrodynamics::Planet.new('moon4',   6, -4,   -2,   20, 0.05, -1e-3, 2e-14,  2e-07)
+  'sun'    => Astrodynamics::Planet.new('sun',     0,  0,    5,  550, 2),
+  'moon1'  => Astrodynamics::Planet.new('moon1',  10, 10,  -20,  890, 0.25),
+  'moon2'  => Astrodynamics::Planet.new('moon2',   6, 18,    2,  500, 0.25),
+  'moon3'  => Astrodynamics::Planet.new('moon3', -10, 10,    8,  400, 0.05),
+  'moon4'  => Astrodynamics::Planet.new('moon4',   6, -4,   -2,  400, 0.05)
 }
 
 # objects['moon1'].make_circular objects['sun']
@@ -35,3 +36,37 @@ CSV.open("planets.csv", "wb") do |csv|
   end
 end
 
+R.eval <<EOF
+  rm(list=ls())
+  library(scatterplot3d)
+  library(animation)
+  library(reshape2)
+
+  x <- read.csv("~/Code/orbital_example/planets.csv", header=FALSE)
+  colnames(x) <- c("t","name","x","y","z")
+
+  UNIQUE <- length(unique(x$name))
+  N <- nrow(x)/UNIQUE
+
+  x <- x[1:(N*UNIQUE), ]
+
+  xl = c(min(x$x)-1, max(x$x)+1)
+  yl = c(min(x$y)-1, max(x$y)+1)
+  zl = c(min(x$z)-1, max(x$z)+1)
+
+  every = 100
+
+  t_list = unique(x$t)
+  t_list = t_list[t_list %% every == 0]
+
+  ani.start()
+  for(i in t_list)
+  {
+   this <- x[x$t == i, ]
+   scatterplot3d(this$x, this$y, this$z, xlab="x", ylab="y", zlab="z", xlim=xl, ylim=yl, zlim=zl)
+  }
+  ani.stop()
+  ani.options(interval = 0.1)
+EOF
+
+sleep(3)
